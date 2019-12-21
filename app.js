231824d53855cf9abdb1f44842ad70d7244bb011
juangalbea <juangalbea@gmail.com>
2019-12-21 19:39:13 +0100

    lifecycle

--- app.js
+++ app.js
@@ -1,65 +1,39 @@
-var data = {
-	title: 'The VueJS Instance',
-  showParagraph: false
-}
-
-Vue.component('hello', {
-	template: '<h1>Hello!</h1>'
-});
-
-var vm1 = new Vue({
-
-  data: data,
-  methods: {
-    show: function() {
-      this.showParagraph = true;
-      this.updateTitle('The VueJS Instance (Updated)');
-      this.$refs.myButton.innerText = 'Text';
-    },
-    updateTitle: function(title) {
-      this.title = title;
-    }
-  },
-  computed: {
-    lowercaseTitle: function() {
-      return this.title.toLowerCase();
-    }
-  },
-  watch: {
-    title: function(value) {
-      alert('Title changed, new value: ' + value);
-    }
-  }
-});
-
-vm1.$mount('#app1');
-
-console.log(vm1.$data === data);
-vm1.$refs.heading.innerText = 'Something else';
-
-setTimeout(function() {
-	vm1.title = 'Changed by timer';
-}, 3000);
-
-
-var vm2 = new Vue({
-	el: '#app2',
+new Vue({
+	el: '#app',
   data: {
-  	title: 'The second Instance'
+  	title: 'The VueJS Instance'
+  },
+  beforeCreate: function() {
+  	console.log('beforeCreate()');
+  },
+  created: function() {
+  	console.log('created()');
+  },
+  beforeMount: function() {
+  	console.log('beforeMount()');
+  },
+  mounted: function() {
+  	console.log('mounted()');
+  },
+  beforeUpdate: function() {
+  	console.log('beforeUpdatee()');
+  },
+  updated: function() {
+  	console.log('updatede()');
+  },
+  beforeDestroy: function() {
+  	console.log('beforeDestroy()');
+  },
+  destroyed: function() {
+  	console.log('destroyed()');
   },
   methods: {
-  	onChange: function() {
-    	vm1.title = 'Changed!';
+  	destroy: function() {
+    	this.$destroy();
     }
   }
-})
-
-var vm3 = new Vue({
-	template: '<h1>Hello!</h1>'
 });
 
-vm3.$mount();
-document.getElementById('app3').appendChild(vm3.$el);
 
 
 
